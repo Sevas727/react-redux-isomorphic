b187f8725916cb77d853096ad15ef760cc05e628
alex <olefirenk@gmail.com>
2016-10-03 12:21:55 +0300

    added dependencies

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router'
-import Main from './Main'
+import Main from './components/Main'
+import App from './pages/App'
+import Event from './pages/Event'
 
 export const routes = (
         <div>
             <Route path='/' component={Main}>
-                <Route path='/events' getComponent={(location, callback) => {
-                    require.ensure([], function (require) {
-                        callback(null, require('./components/App').default);
-                    });
-                }}/>
-                <Route path='/event:id' getComponent={(location, callback) => {
-                    require.ensure([], function (require) {
-                        callback(null, require('./components/Event').default);
-                    });
-                }}/>
+                <Route path='/events' component={App}/>
+                <Route path='/event:id' component={Event}/>
             </Route>
         </div>
     )
